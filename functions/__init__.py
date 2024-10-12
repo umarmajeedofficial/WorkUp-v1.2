3d653d17a145288833fd9da78e70ddcbc263ede1
Umar Majeed <121746394+umarmajeedofficial@users.noreply.github.com>
2024-10-13 03:31:47 +0500

    Update __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -6,7 +6,6 @@ from .flowchart_generation import generate_flowchart
 from .project_structure_generation import generate_project_structure
 from .project_naming import suggest_project_names
 from .utils import extract_text    
-from .project_files_structure import generate_project_files_structure
 from config import API_KEY, BASE_URL, MODEL_NAME
 from openai import OpenAI
 
