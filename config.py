617a4ce346dbe056bd35b0ec7edda8ee1e2831a0
Umar Majeed <121746394+umarmajeedofficial@users.noreply.github.com>
2024-10-13 00:30:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
+# config.py
 
+API_KEY = "8772096b1b3248128cf4072be826ee90"
+BASE_URL = "https://api.aimlapi.com"
+MODEL_NAME = "meta-llama/Llama-3.2-3B-Instruct-Turbo"
+
+# Add any other global configurations here
