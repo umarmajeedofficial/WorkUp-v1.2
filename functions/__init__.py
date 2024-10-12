dd36ca3f5fc597e4625fe5a600dc5bf8fc564708
Umar Majeed <121746394+umarmajeedofficial@users.noreply.github.com>
2024-10-13 00:31:12 +0500

    Update __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,1 +1,13 @@
+# functions/__init__.py
+
+from .workload_distribution import get_workload_distribution
+from .project_workflow import get_project_workflow
+from .flowchart_generation import generate_flowchart
+from .project_structure_generation import generate_project_structure
+from .project_naming import suggest_project_names
+from .utils import extract_text
+from config import API_KEY, BASE_URL, MODEL_NAME
+from openai import OpenAI
+
+client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
 
