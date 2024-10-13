aca3362a7a0fa790e9ce5ae4fbbd24efca9af260
Umar Majeed <121746394+umarmajeedofficial@users.noreply.github.com>
2024-10-13 12:18:30 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
+import streamlit as st
+
 # config.py
 
-API_KEY = "8772096b1b3248128cf4072be826ee90"
-BASE_URL = "https://api.aimlapi.com"
-MODEL_NAME = "meta-llama/Llama-3.2-3B-Instruct-Turbo"
-
-# Add any other global configurations here
+API_KEY = st.secrets["api_config"]["api_key"]
+BASE_URL = st.secrets["api_config"]["base_url"]
+MODEL_NAME = st.secrets["api_config"]["model_name"]
